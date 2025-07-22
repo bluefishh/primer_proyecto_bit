e41db797f09c9f5fa02c29facab2d595768325f8
bluefish <edycasa45@gmail.com>
2025-07-21 22:11:46 -0500

    Se agrega búsqueda de imágenes/fotografías, la búsqueda se realiza por título o autor

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalEl = document.getElementById('imagenModal');
     const bsModal = new bootstrap.Modal(modalEl);
 
-    // Se agrega evento de clic a cada tarjeta
+    // Se agrega evento de clic a cada tarjeta para poder ver la imagen en un modal con el título, autor y descripción
     document.querySelectorAll('.card').forEach(card => {
         card.addEventListener('click', () => {
             // Se obtienen los elementos necesarios de la tarjeta
@@ -26,6 +26,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Se muestra el modal
             bsModal.show();
+        });
+    });
+
+    // Se agrega evento para la búsqueda de imágenes
+    document.getElementById('inputBuscar').addEventListener('input', (event) => {
+        // Se obtiene el criterio de búsqueda
+        const buscarString = event.target.value.toLowerCase();
+        
+        // Se busca en todos los cards con imágenes
+        document.querySelectorAll('.card').forEach(card => {
+            const title = card.querySelector('.card-title').textContent.toLowerCase();
+            const author = card.querySelector('.card-subtitle').textContent.toLowerCase();
+
+            // Se valida el criterio de búsqueda en el título y autor de la imagen
+            if (title.includes(buscarString) || author.includes(buscarString)) {
+                // En caso de coincidir se mantiene visible el card
+                card.style.display = 'block';
+            } else {
+                // En caso de que no coincida oculta el card
+                card.style.display = 'none';
+            }
         });
     });
 });
